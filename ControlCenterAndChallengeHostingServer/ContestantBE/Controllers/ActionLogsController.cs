640547cf9a66a83d2436decebdf489e9768893ec
DuongQuyen <minhquyen130904@gmail.com>
2026-04-26 22:40:49 +0700

    update ActionLogsController temp

--- ControlCenterAndChallengeHostingServer/ContestantBE/Controllers/ActionLogsController.cs
+++ ControlCenterAndChallengeHostingServer/ContestantBE/Controllers/ActionLogsController.cs
@@ -21,8 +21,8 @@ public class ActionLogsController : BaseController
     [HttpGet("get-logs-team")]
     public async Task<IActionResult> GetActionLogsTeam()
     {
-        var teamId = UserContext.TeamId;
-        var logs_with_details = await _actionLogsServices.GetActionLogsTeam(teamId);
+        var contestId = UserContext.ContestId;
+        var logs_with_details = await _actionLogsServices.GetActionLogsTeam(teamId, contestId);
 
         if (logs_with_details == null || logs_with_details.Count == 0)
         {
